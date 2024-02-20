391ae43bcb1a25e8edeafb70033233a6ae59d54e
Roy Aringoy <roeder.aringoy@gmail.com>
2024-02-21 00:06:06 +0800

    Initial Commit

---
+++ src/main/java/dev/roy/springcustomannotations/SpringCustomAnnotationsApplication.java
@@ -0,0 +1,13 @@
+package dev.roy.springcustomannotations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCustomAnnotationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCustomAnnotationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/roy/springcustomannotations/SpringCustomAnnotationsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.roy.springcustomannotations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCustomAnnotationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
